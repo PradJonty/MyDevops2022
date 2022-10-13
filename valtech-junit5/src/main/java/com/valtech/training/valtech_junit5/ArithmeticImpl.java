8e151eb8a3bd84667786799ce51e2c42f44fca93
R D Pradeekar <Pradeekar.RD@valtech.com>
2022-10-13 15:51:13 +0530

    updatedjunittest

---
+++ valtech-junit5/src/main/java/com/valtech/training/valtech_junit5/ArithmeticImpl.java
@@ -0,0 +1,20 @@
+package com.valtech.training.valtech_junit5;
+public class ArithmeticImpl implements Arithmetic{
+
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+	public int sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return a-b;
+	}
+	public int mul(int a, int b) {
+		// TODO Auto-generated method stub
+		return a*b;
+	}
+	public int div(int a, int b) {
+		// TODO Auto-generated method stub
+		return a/b;
+	}
+}
